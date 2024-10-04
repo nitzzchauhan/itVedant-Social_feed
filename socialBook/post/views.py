a166dc75fbf480796ef29fb2df56c67a8bfc841b
nitin chauhan <nitinkiranchauhan@gmail.com>
2024-10-04 10:37:11 +0530

    new file

---
+++ socialBook/post/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+
+
+
+# Create your views here.
+
+def index(request):
+    # return HttpResponse("dskjgndkjsgnk")
+    return render(request, 'layout.html')
